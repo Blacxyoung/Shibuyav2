c1f64334d2279b8fc01044d54a99e6e9328c13da
Burraku-Black <72658799+Blacxyoung@users.noreply.github.com>
2021-05-14 14:05:06 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,91 @@
+const help = (prefix) => {
+	return `
+「 *ShanBot by shanduy* 」
+
+◪ *Información*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Creador: Shanduy
+  ❏ Como instalar el bot: https://www.youtube.com/watch?v=2LQSzEbpJ-M
+  ❏ Instagram: https://www.instagram.com/thepavos
+
+◪ *Para Usar Al Bot*
+  ❏ Registrate con el comando ${prefix}daftar y tu nombre
+
+◪ *Nueva Actualización*
+  │
+  ├─ ❏ ${prefix}play
+  └─ Ahora funciona con links de youtube
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}ping
+  └─ Estado de tu internet "Broma"
+
+◪ *CREAR STICKERS*
+  │
+  ├─ ❏ ${prefix}sticker o s
+  ├─ ❏ ${prefix}stickergif
+  ├─ 6 segundos de video
+  ├─ ❏ ${prefix}toimg
+  ├─ De sticker a JPG
+  ├─ ❏ ${prefix}tomp3
+  └─ De video a MP3
+
+◪ *DESCARGAS*
+  │
+  ├─ ❏ ${prefix}ytmp4
+  └─ Mas link del video de YT
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ Mas nombre y artista
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts es
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame
+  ├─ Link de Whatsapp
+  ├─ ❏ ${prefix}troleo
+  ├─ trolea a tus panas
+  ├─❏ ${prefix}qrcode
+  └─ Coloca un texto
+
+◪ *GRUPOS*
+  │
+  ├─ ❏ ${prefix}welcome 1
+  ├─ Bienvenida a los miembros
+  ├─ ❏ ${prefix}closecg
+  ├─ Cerrar el grupo solo admins
+  ├─ ❏ ${prefix}opencg
+  ├─ Abrir grupo solo admins
+  ├─ ❏ ${prefix}kick o pafuera
+  ├─ Eliminar a un miembro 
+  ├─ ❏ ${prefix}promote
+  ├─ Dar admin a un miembro
+  ├─ ❏ ${prefix}demote
+  ├─ Quitar el admin
+  ├─ ❏ ${prefix}linkgc
+  ├─ Link del grupo
+  ├─ ❏ ${prefix}tagall
+  └─ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+◪ *NSWF NUEVAS FUNCIONES* 
+  │
+  └─ ❏ ${prefix}nsfwmenu
+
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+
+No te olvides de seguirme en instagram flaco :)
+
+Para saber la ultima version mas actual del bot entra en 
+https://github.com/shanduy/ShanBot
+
+by shanduy
+`
+}
+
+exports.help = help
